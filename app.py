2139645c6ab8a89444510acbc0ce61c4d99b32f5
dev-jey <savalijey0@gmail.com>
2020-08-09 23:42:42 +0300

    fix the login system

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 from flask import Flask, render_template, request
-import pickle
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 app = Flask(__name__)
 
 
 options = webdriver.ChromeOptions()
 
-options.add_experimental_option("detach", True)
-options.add_argument("user-data-dir=myselenium")
+# options.add_experimental_option("detach", True)
+# options.add_argument("user-data-dir=myselenium")
 
 
 
@@ -21,25 +26,34 @@ def home():
 
 @app.route("/get")
 def start_messaging():
-    # driver.get("https://web.telegram.org/")
     link = request.args.get('link')
     message = request.args.get('msg')
     duration = request.args.get('duration')
     start_browser(link, message, duration)
 
-
 def start_browser(link, message, duration):
     global driver
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    # options.add_argument("--headless")  
+    wait =  WebDriverWait(driver, 10000)
     driver.maximize_window()
+    driver.get('https://web.telegram.org/#/login')
+    wait.until(
+        lambda driver: driver.current_url == 'https://web.telegram.org/#/login')
+    wait.until(
+        lambda driver: driver.current_url == 'https://web.telegram.org/#/im')
+    time.sleep(5)
     driver.get(link)
-    driver.implicitly_wait(20)
-    while True: 
-        time.sleep(int(duration))
-        textarea = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[2]/div[5]").send_keys(message)
-        driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[3]/button").click()
-    
+    wait.until(
+        lambda driver: driver.current_url == link)
+    if link == driver.current_url:
+        while True: 
+            time.sleep(int(duration))
+            textarea = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[2]/div[5]").send_keys(message)
+            driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[3]/button").click()
+    else:
+         driver.quit()
+
+
 def close_windows():
     driver.close()
     driver.quit()
