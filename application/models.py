ebca858f9244001953edc2e2b9a79fc42f0f846f
dev-jey <savalijey0@gmail.com>
2020-08-10 20:24:28 +0300

    add db connection

---
+++ application/models.py
@@ -0,0 +1,57 @@
+"""Data models."""
+from . import db
+
+
+class User(db.Model):
+    """Data model for user accounts."""
+
+    __tablename__ = 'flasksqlalchemy-tutorial-users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    email = db.Column(
+        db.String(80),
+        index=True,
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.Text,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    created = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    admin = db.Column(
+        db.Boolean,
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    paid = db.Column(
+        db.Boolean,
+        index=False,
+        unique=False,
+        nullable=False
+    )
+    active = db.Column(
+        db.Boolean,
+        index=False,
+        unique=False,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
