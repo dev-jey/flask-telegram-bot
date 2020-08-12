c7a138aad490ef3ee2ede14050937191fcdb2fe1
dev-jey <savalijey0@gmail.com>
2020-08-12 18:03:48 +0300

    fix app run

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from application import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=9090, debug=True)
