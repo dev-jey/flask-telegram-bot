9d3fd4cfee5182d1b3d9b1e1459121368e03f0f3
dev-jey <savalijey0@gmail.com>
2020-08-15 14:54:15 +0300

    fix

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from application import create_app
-app = create_app()
+# app = create_app()
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
-    # app.run()
+    app.run(host='0.0.0.0', port=80)
